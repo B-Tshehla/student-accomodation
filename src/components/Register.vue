9f1ebefbcb3d5a8b5f8aaf88f0851b46feddbe12
Boitumelo <boitumelotshehla@gmaail.com>
2021-09-05 18:21:47 +0200

    v1.0.0.0

---
+++ src/components/Register.vue
@@ -0,0 +1,42 @@
+<template>
+
+    <form >
+        <h3>Sign Up</h3>
+
+            <div class="form-group">
+
+            <label>Student Number</label>
+            <input type="text" class="form-control" placeholder="Last Name">
+        </div>
+
+        <div class="form-group">
+
+            <label>Email</label>
+            <input type="email" class="form-control" placeholder="Last Name">
+        </div>
+
+        <div class="form-group">
+
+            <label>Password</label>
+            <input type="password" class="form-control" placeholder="password">
+        </div>
+
+        <div class="form-group">
+
+            <label>Cornfirm Password</label>
+            <input type="text" class="form-control" placeholder="Cornfrim Password">
+        </div>
+
+        <button class="btn btn-primary btn-block">
+            Sign Up
+        </button>
+
+    </form>
+
+</template>
+
+<script>
+export default {
+    name: 'Register'
+}
+</script>
